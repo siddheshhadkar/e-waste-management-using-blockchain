f296055c6bc178e7e88469ab6837a0382e068baa
siddheshhadkar <siddheshhadkar07@gmail.com>
2020-03-05 01:18:42 +0530

    Started working in App.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 App = {
     web3Provider: null,
     contracts: {},
+    contractAddresses: {},
     account: '0x0',
 
     init: function(){
@@ -22,108 +23,72 @@ App = {
         $.getJSON('AddressManager.json', function(addressManager){
             App.contracts.AddressManager = TruffleContract(addressManager);
             App.contracts.AddressManager.setProvider(App.web3Provider);
+            return App.initAddresses('AddressManager');
         });
         $.getJSON('CollectionCentre.json', function(collectionCentre){
             App.contracts.CollectionCentre = TruffleContract(collectionCentre);
             App.contracts.CollectionCentre.setProvider(App.web3Provider);
+            return App.initAddresses('CollectionCentre');
         });
         $.getJSON('Consumer.json', function(consumer){
             App.contracts.Consumer = TruffleContract(consumer);
             App.contracts.Consumer.setProvider(App.web3Provider);
+            return App.initAddresses('Consumer');
         });
         $.getJSON('Producer.json', function(producer){
             App.contracts.Producer = TruffleContract(producer);
             App.contracts.Producer.setProvider(App.web3Provider);
+            return App.initAddresses('Producer');
         });
         $.getJSON('Retailer.json', function(retailer){
             App.contracts.Retailer = TruffleContract(retailer);
             App.contracts.Retailer.setProvider(App.web3Provider);
+            return App.initAddresses('Retailer');
         });
         $.getJSON('RecycleUnit.json', function(recycleUnit){
             App.contracts.RecycleUnit = TruffleContract(recycleUnit);
             App.contracts.RecycleUnit.setProvider(App.web3Provider);
+            return App.initAddresses('RecycleUnit');
         });
-        // return App.render();
-        return App.initAddresses();
     },
 
-    initAddresses: function(){
-        console.log('reached');
+    initAddresses: function(contractName){
+        App.contracts[contractName].deployed()
+            .then((i) => i.address)
+            .then(function(address){
+                App.contractAddresses[contractName] = address;
+                if (Object.keys(App.contractAddresses).length == 6) {
+                    return App.setAddressManagerAddresses();
+                }
+            });
+    },
+
+    setAddressManagerAddresses: function(){
+        // console.log(App.contractAddresses);
+        for (var contractName in App.contractAddresses) {
+            App.contracts.AddressManager.deployed()
+                .then(function(i){
+                    console.log(contractName, App.contractAddresses[contractName]);
+                    // i.setAddress(contractName, App.contractAddresses[contractName]);
+                    // console.log(i.getAddress(contractName));
+                });
+        }
     },
 
     listenForEvents: function(){
-        App.contracts.Election.deployed().then(function(i){
-            i.votedEvent({}, {
-                fromBlock:0,
-                toBlock: 'latest'
-            }).watch(function(){
-                console.log("Event triggered", event);
-                App.render();
-            });
-        });
+
     },
 
     render: function(){
-        var electionInstance;
-        var loader = $('#loader');
-        var content = $('#content');
+        for (var key in App.contractAddresses) {
+            console.log(key, App.contractAddresses[key]);
+        }
 
-        loader.show();
-        content.hide();
-
-        //Loading account data
         web3.eth.getCoinbase(function(err, account){
             if(err===null){
                 App.account = account;
-                $('#accountAddress').html("Your account address is: "+App.account);
+                $('#petsRow').html("Your account address is: " + App.account);
             }
-        });
-
-        //Loading contract data
-        App.contracts.Election.deployed().then(function(i){
-            electionInstance = i;
-            return electionInstance.candidatesCount();
-        }).then(function(candidatesCount){
-            var candidatesResults = $('#candidatesResults');
-            candidatesResults.empty();
-
-            var candidatesSelect = $('#candidatesSelect');
-            candidatesSelect.empty();
-
-            for(var i = 1; i<=candidatesCount; i++){
-                electionInstance.candidates(i).then(function(candidate){
-                    var id = candidate[0].toNumber();
-                    var name = candidate[1];
-                    var voteCount = candidate[2].toNumber();
-
-                    var candidateTemplate = "<tr><th>"+id+"</th><td>"+name+"</td><td>"+voteCount+"</td></tr>";
-                    candidatesResults.append(candidateTemplate);
-
-                    var candidateOption = "<option value='"+id+"'>"+name+"</option>";
-                    candidatesSelect.append(candidateOption);
-                });
-            }
-            return electionInstance.voters(App.account);
-        }).then(function(hasVoted){
-            if(hasVoted){
-                $('form').hide();
-            }
-            loader.hide();
-            content.show();
-        }).catch(function(err){
-            console.warn(err);
-        });
-    },
-
-    castVote: function(){
-        var candidateId = $('#candidatesSelect').val();
-        App.contracts.Election.deployed().then(function(i){
-            return i.vote(candidateId, {from: App.account});
-        }).then(function(result){
-            $('#content').hide();
-            $('#loader').show();
-        }).catch(function(err){
-            console.error(err);
         });
     }
 };
