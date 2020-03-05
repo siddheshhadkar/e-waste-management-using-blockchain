d7ff4c34533a5459c836e864b54e1303dc95d85a
siddheshhadkar <siddheshhadkar07@gmail.com>
2020-03-06 00:27:42 +0530

    Migrations moved to a single file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,18 @@
 var AddressManager = artifacts.require("./AddressManager.sol");
+var CollectionCentre = artifacts.require("./CollectionCentre.sol");
+var Consumer = artifacts.require("./Consumer.sol");
+var Producer = artifacts.require("./Producer.sol");
+var RecycleUnit = artifacts.require("./RecycleUnit.sol");
+var Retailer = artifacts.require("./Retailer.sol");
 
-module.exports = function(deployer) {
-    deployer.deploy(AddressManager);
+module.exports = async function(deployer) {
+    await deployer.deploy(AddressManager);
+
+    address = AddressManager.address;
+
+    await deployer.deploy(CollectionCentre, address);
+    await deployer.deploy(Consumer, address);
+    await deployer.deploy(Producer, address);
+    await deployer.deploy(RecycleUnit, address);
+    await deployer.deploy(Retailer, address);
 };
