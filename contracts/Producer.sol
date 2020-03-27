601de8bab9fce28741aea0897da3f9b82465cfe8
Heramb <44063354+Hearmb@users.noreply.github.com>
2020-03-27 19:47:03 +0530

    Producer contract&profile updated

--- contracts/Producer.sol
+++ contracts/Producer.sol
@@ -5,65 +5,81 @@ import "./AddressManager.sol";
 contract Producer {
 
 	mapping (uint => Product) public newProducts;
-	mapping (uint => Product) public returnedProducts;
+// 	mapping (uint => Product) public returnedProducts;
 
-	address owner;
-	address producer;
-	AddressManager amInstance;
+    address owner;
+	// AddressManager amInstance;
 	uint newProductsCount;
-	uint returnedProductsCount;
+// 	uint returnedProductsCount;
 
 
 	modifier onlyOwner(){
-        require(msg.sender == owner);
-        _;
-    }
+         require(msg.sender == owner);
+         _;
+     }
 
-    modifier onlyProducer(){
-        require(msg.sender == producer);
-        _;
-    }
+    // 0 weightOfGlass;
+    // 1 weightOfPlastic;
+    // 2 weightOfNickel;
+    // 3 weightOfAluminium;
+    // 4 weightOfCopper;
+    // 5 weightOfMagnesium;
+    // 6 weightOfLead;
+   
 
     struct Product {
         address producerAddress;
         address retailerAddress;
         address consumerAddress;
-        address collectionAddress;
         string name;
         string typeOfProduct;
-        uint weightOfGlass;
-        uint weightOfPlastic;
-        uint weightOfNickel;
-        uint weightOfAluminium;
-        uint weightOfCopper;
-        uint weightOfMagnesium;
-        uint weightOfLead;
-        // bool recycled;
+        bool returnedToRetailer;
+        bool returnedToProcuder;
+        uint reusePercentage;
+        uint[7] weights;
     }
 
 	constructor(address _addressManager) public {
-    	owner=msg.sender;
+        owner=msg.sender;
     	amInstance=AddressManager(address(_addressManager));
 	}
 
 	function addProduct(string memory _name,string memory _type,
 		uint _weightOfAluminium, uint _weightOfNickel, uint _weightOfGlass, uint _weightOfPlastic,
 		uint _weightOfCopper, uint _weightOfMagnesium, uint _weightOfLead) public  {
+    
+        uint[7] memory temp;
 
+        temp[0]=_weightOfGlass;
+        temp[1]=_weightOfPlastic;
+        temp[2]=_weightOfNickel;
+        temp[3]=_weightOfAluminium;
+        temp[4]=_weightOfCopper;
+        temp[5]=_weightOfMagnesium;
+        temp[6]=_weightOfLead;
+        
 		newProductsCount++;
 		newProducts[newProductsCount]=Product(msg.sender,
-			0x0000000000000000000000000000000000000000,
-			0x0000000000000000000000000000000000000000,
-			0x0000000000000000000000000000000000000000,
+			address(0),
+			address(0),
 			_name,_type,
-			_weightOfGlass,_weightOfPlastic,_weightOfNickel,_weightOfAluminium,_weightOfCopper
-			,_weightOfMagnesium,_weightOfLead);
+			false,false,0,temp);
 	}
 
-	function addReturnProduct(uint _id) public onlyProducer{
-		returnedProductsCount++;
-		returnedProducts[returnedProductsCount] = newProducts[_id];
+	function addReturnProduct(uint _id) public {
+	    newProducts[_id].returnedToProcuder=true;
 	}
-
+	
+	function soldToRetailer(uint _id,address _retailer) public{
+	    newProducts[_id].retailerAddress=_retailer;
+	}
+	
+	function soldToConsumer(uint _id,address _consumer) public{
+	    newProducts[_id].consumerAddress=_consumer;
+	}
+    
+    function addPercentage(uint _id,uint _percentage) public{
+        newProducts[_id].reusePercentage=_percentage;
+    }
 
 }
