154de58879939cd81f3903af85e665f9f6f893a8
siddheshhadkar <siddheshhadkar07@gmail.com>
2020-03-31 12:34:24 +0530

    Trivial trivial changes

--- contracts/Producer.sol
+++ contracts/Producer.sol
@@ -4,19 +4,13 @@ import "./AddressManager.sol";
 
 contract Producer {
 
-	mapping (uint => Product) public newProducts;
-// 	mapping (uint => Product) public returnedProducts;
-
     address owner;
-		AddressManager amInstance;
-		uint public newProductsCount;
-// 	uint returnedProductsCount;
+	AddressManager amInstance;
 
-
-		modifier onlyOwner(){
+	modifier onlyOwner(){
          require(msg.sender == owner);
          _;
-    }
+     }
 
     // 0 weightOfGlass;
     // 1 weightOfPlastic;
@@ -25,7 +19,6 @@ contract Producer {
     // 4 weightOfCopper;
     // 5 weightOfMagnesium;
     // 6 weightOfLead;
-
 
     struct Product {
         address producerAddress;
@@ -39,12 +32,15 @@ contract Producer {
         uint[7] weights;
     }
 
-		constructor(address _addressManager) public {
-        owner=msg.sender;
-    		amInstance=AddressManager(address(_addressManager));
-		}
+    Product[] public newProducts;
+    Product[] public returnedProducts;
 
-		function addProduct(string memory _name,string memory _type,
+	constructor(address _addressManager) public {
+        owner=msg.sender;
+    	amInstance=AddressManager(address(_addressManager));
+	}
+
+	function addProduct(string memory _name,string memory _type,
 		uint _weightOfAluminium, uint _weightOfNickel, uint _weightOfGlass, uint _weightOfPlastic,
 		uint _weightOfCopper, uint _weightOfMagnesium, uint _weightOfLead) public  {
 
@@ -58,27 +54,40 @@ contract Producer {
         temp[5]=_weightOfMagnesium;
         temp[6]=_weightOfLead;
 
-		newProductsCount++;
-		newProducts[newProductsCount]=Product(msg.sender,
+		newProducts.push(Product(msg.sender,
 			address(0),
 			address(0),
 			_name,_type,
-			false,false,0,temp);
-		}
+			false,false,0,temp));
+	}
 
-		function addReturnProduct(uint _id) public {
-	    	newProducts[_id].returnedToProducer=true;
-		}
+	function getNewProductCount() public view returns(uint){
+	    return newProducts.length;
+	}
 
-		function soldToRetailer(uint _id,address _retailer) public{
-		    newProducts[_id].retailerAddress=_retailer;
-		}
+	function getReturnedProductCount() public view returns(uint){
+	    return returnedProducts.length;
+	}
 
-		function soldToConsumer(uint _id,address _consumer) public{
-		    newProducts[_id].consumerAddress=_consumer;
-		}
+	function addReturnProduct(uint _id) public {
+	    newProducts[_id].returnedToProducer=true;
+	}
+
+	function soldToRetailer(uint _id,address _retailer) public{
+	    newProducts[_id].retailerAddress=_retailer;
+	}
+
+	function soldToConsumer(uint _id,address _consumer) public{
+	    newProducts[_id].consumerAddress=_consumer;
+	}
 
     function addPercentage(uint _id,uint _percentage) public{
         newProducts[_id].reusePercentage=_percentage;
     }
+
+    //Consumer Methods
+    function fetchProductReferenceConsumer(address _address) public view returns(uint){
+
+    }
+
 }
